51bab1b401431934538e2e6364eb4b582ad2e913
Joon Lee <joon.lee@prodigiq.com>
2018-06-13 17:53:02 -0700

    add vuex files to ease adding store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import user from './modules/user'
-import team from './modules/team'
+import modules from './modules'
 
 Vue.use(Vuex)
 
-const debug = process.env.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
-  modules: {
-    user,
-    team
-  },
-  strict: debug
+  modules,
+  strict: process.env.NODE_ENV !== 'production'
 })
