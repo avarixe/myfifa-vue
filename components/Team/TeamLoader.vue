e1c22330f61a93f8aa1aaf71dd237267fad1184e
Joon Lee <joon.lee@ucla.edu>
2018-11-27 09:41:24 -0800

    WIP: Add TeamLoader component

---
+++ components/Team/TeamLoader.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="loading-page">
+    <v-progress-circular
+      :size="200"
+      :width="15"
+      color="primary"
+      :value="value"
+      class="text-xs-center primary--text"
+    >{{ status }}</v-progress-circular>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    props: {
+      team: Object
+    },
+    data: () => ({
+      value: 0,
+      loaded: false,
+      status: 'Loading Team Data'
+    }),
+    async mounted () {
+      const loaders = [
+        { resource: 'Players',      handler: this.getPlayers },
+        { resource: 'Matches',      handler: this.getMatches },
+        { resource: 'Squads',       handler: this.getSquads },
+        { resource: 'Competitions', handler: this.getCompetitions }
+      ]
+
+      for (let i = 0; i < loaders.length; i++) {
+        await this.loadData(loaders[i])
+        this.value = 100 / loaders.length * (i + 1)
+      }
+
+      this.loaded = true
+    },
+    watch: {
+      loaded (val) {
+        this.$emit('loaded')
+      }
+    },
+    methods: {
+      ...mapActions({
+        getPlayers: 'player/getAll',
+        getMatches: 'match/getAll',
+        getSquads: 'squad/getAll',
+        getCompetitions: 'competition/getAll'
+      }),
+      async loadData ({ resource, handler }) {
+        this.status = `Loading ${resource}...`
+        await handler({ teamId: this.team.id })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .loading-page {
+    display: flex;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    background: rgba(255, 255, 255, 0.5);
+    z-index: 2;
+  }
+</style>
