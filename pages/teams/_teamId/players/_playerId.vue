456bf703c58712e273feab08f454af5d0cd24fe8
Joon Lee <l33j00n325@gmail.com>
2019-06-04 21:27:51 -0700

    fix issue where Injuries did not show in Player page

--- pages/teams/_teamId/players/_playerId.vue
+++ pages/teams/_teamId/players/_playerId.vue
@@ -326,7 +326,7 @@
       player () {
         return Player
           .query()
-          .with('contracts|loans|injures|transfers|histories')
+          .with('contracts|loans|injuries|transfers|histories')
           .find(this.$route.params.playerId)
       },
       active () { return this.player.status && this.player.status.length > 0 },
