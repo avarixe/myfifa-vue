94ab9f82e69b36be03a7f05485f2b3853cad06f5
Joon Lee <l33j00n325@gmail.com>
2020-01-13 18:13:18 -0800

    redirect back to original path after authentication

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -4,6 +4,6 @@ export default function ({ store, route, redirect }) {
       return redirect('/teams')
     }
   } else if (route.name !== 'index') {
-    return redirect('/')
+    return redirect(`/#${route.path}`)
   }
 }
