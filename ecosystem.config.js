d322844f4f083a771de7716ed1b3304f8ab93622
Joon Lee <l33j00n325@gmail.com>
2020-10-01 18:09:13 -0700

    add PM2 deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'myfifa',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
