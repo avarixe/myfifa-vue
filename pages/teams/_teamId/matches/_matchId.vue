cf182bc8e232ffa91574aa2569f1098952f7ac01
Joon Lee <l33j00n325@gmail.com>
2020-11-07 20:10:05 -0800

    fix issue where Squads were not loaded upon initial MatchPage render

--- pages/teams/_teamId/matches/_matchId.vue
+++ pages/teams/_teamId/matches/_matchId.vue
@@ -232,7 +232,7 @@
       ])
 
       if (!this.readonly) {
-        this.fetchSquads({ teamId: this.team.id })
+        await this.fetchSquads({ teamId: this.team.id })
       }
 
       this.setPage({
