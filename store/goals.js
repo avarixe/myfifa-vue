cdea6efec307de21db45ac01d14f1bd0f0a823ea
Joon Lee <l33j00n325@gmail.com>
2021-01-17 15:34:50 -0800

    apply axios module to match and match event stores

--- store/goals.js
+++ store/goals.js
@@ -1,22 +1,21 @@
-import { crud, http, routes } from '@/api'
 import { Goal } from '@/models'
 
 // actions
 export const actions = {
-  ...crud({
-    model: Goal,
-    parent: 'match'
-  }),
-  SEARCH ({ rootState }, { teamId, filters }) {
-    return http({
-      method: 'post',
-      path: routes.goals.search,
-      pathData: { teamId },
-      data: { filters },
-      token: rootState.token,
-      success ({ data }) {
-        Goal.insert({ data })
-      }
+  async CREATE (_, { matchId, goal }) {
+    const data = await this.$axios.$post(`matches/${matchId}/goals`, {
+      goal
     })
+    Goal.insert({ data })
+  },
+  async UPDATE (_, goal) {
+    const data = await this.$axios.$patch(`goals/${goal.id}`, {
+      goal
+    })
+    Goal.insert({ data })
+  },
+  async REMOVE (_, goalId) {
+    await this.$axios.$delete(`goals/${goalId}`)
+    Goal.delete(goalId)
   }
 }
