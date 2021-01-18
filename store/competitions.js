02a428c54d0c42bb7c59ca7467007e4973f5ed5f
Joon Lee <l33j00n325@gmail.com>
2021-01-17 16:37:10 -0800

    apply axios module to competition stores

--- store/competitions.js
+++ store/competitions.js
@@ -1,8 +1,30 @@
-import { crud } from '@/api'
 import { Competition } from '@/models'
 
 // actions
-export const actions = crud({
-  model: Competition,
-  parent: 'team'
-})
+export const actions = {
+  async FETCH (_, { teamId }) {
+    const data = await this.$axios.$get(`teams/${teamId}/competitions`)
+    Competition.insert({ data })
+  },
+  async GET (_, { competitionId }) {
+    const data = await this.$axios.$get(`competitions/${competitionId}`)
+    Competition.insert({ data })
+  },
+  async CREATE (_, { teamId, competition }) {
+    const data = await this.$axios.$post(`teams/${teamId}/competitions`, {
+      competition
+    })
+    Competition.insert({ data })
+    return data
+  },
+  async UPDATE (_, competition) {
+    const data = await this.$axios.$patch(`competitions/${competition.id}`, {
+      competition
+    })
+    Competition.insert({ data })
+  },
+  async REMOVE (_, competitionId) {
+    await this.$axios.$delete(`competitions/${competitionId}`)
+    Competition.delete(competitionId)
+  }
+}
--- store/tableRows.js
+++ store/tableRows.js
@@ -1,8 +1,21 @@
-import { crud } from '@/api'
 import { TableRow } from '@/models'
 
 // actions
-export const actions = crud({
-  model: TableRow,
-  parent: 'stage'
-})
+export const actions = {
+  async CREATE (_, { stageId, tableRow }) {
+    const data = await this.$axios.$post(`stages/${stageId}/table_rows`, {
+      table_row: tableRow
+    })
+    TableRow.insert({ data })
+  },
+  async UPDATE (_, tableRow) {
+    const data = await this.$axios.$patch(`table_rows/${tableRow.id}`, {
+      table_row: tableRow
+    })
+    TableRow.insert({ data })
+  },
+  async REMOVE (_, tableRowId) {
+    await this.$axios.$delete(`table_rows/${tableRowId}`)
+    TableRow.delete(tableRowId)
+  }
+}
