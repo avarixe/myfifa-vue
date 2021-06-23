3260a37ef1c34de2ba953699fb2dc50257c0df47
Joon Lee <l33j00n325@gmail.com>
2021-06-22 20:17:41 -0700

    fix issue where CapForm was not rendered by SSR

--- components/Match/MatchActions.vue
+++ components/Match/MatchActions.vue
@@ -3,6 +3,7 @@
     <cap-form
       v-if="match.caps.length < 11"
       :match="match"
+      class="d-inline-block"
     />
     <match-squad-applier :match="match" />
     <match-squad-saver :match="match" />
